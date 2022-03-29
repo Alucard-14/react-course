9a26c5fdd46f7ab2d307745b377066b35dfb7455
Alucard-14 <101140540+Alucard-14@users.noreply.github.com>
2022-03-29 09:09:50 -0500

    Added dynamic CSS classnames

--- styling-test/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-test/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,25 +3,36 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [validInput, setValidInput] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const formControlCSSValidation = `form-control ${!validInput && 'invalid'}`;
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) setValidInput(true);
+
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setValidInput(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={formControlCSSValidation}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type='text' onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };

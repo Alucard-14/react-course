e1b7941860108541e0c2bb26dcbc8e9826bf3270
Alucard-14 <101140540+Alucard-14@users.noreply.github.com>
2022-03-29 10:12:37 -0500

    Change styled components to css modules logic

--- styling-test/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-test/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,39 +2,39 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ invalid }) => (invalid ? 'red' : 'inherit')};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({ invalid }) => (invalid ? 'red' : 'inherit')};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ invalid }) => (invalid ? 'red' : '#ccc')};
-    background: ${({ invalid }) =>
-      invalid ? 'rgb(245, 193, 193)' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({ invalid }) => (invalid ? 'red' : '#ccc')};
+//     background: ${({ invalid }) =>
+//       invalid ? 'rgb(245, 193, 193)' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -59,10 +59,13 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!validInput}>
+      <div
+        className={`${styles['form-control']} ${
+          !validInput && styles.invalid
+        }`}>
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type='submit'>Add Goal</Button>
     </form>
   );

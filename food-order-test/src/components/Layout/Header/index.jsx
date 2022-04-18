7193f8ea8de917ee83f3b35675e0fc5ed66dd411
Alucard-14 <101140540+Alucard-14@users.noreply.github.com>
2022-04-18 08:38:30 -0500

    Added Header Component

---
+++ food-order-test/src/components/Layout/Header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import mealImg from '../../../assets/meals.jpg';
+
+const Header = () => {
+  return (
+    <>
+      <header className='fixed top-0 left-0 w-full h-20 bg-[#8a2b06] text-white flex justify-between items-center py-0 px-[10%] shadow-[0_2px_8px_rgba(0,0,0,0.25)] z-10'>
+        <h1>Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className='w-full h-[25rem] z-0 overflow-hidden'>
+        <img
+          className='w-full h-full object-cover -rotate-[5deg] -translate-y-20 -translate-x-4'
+          src={mealImg}
+          alt='buffet'
+        />
+      </div>
+    </>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
